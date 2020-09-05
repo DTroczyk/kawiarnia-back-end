bece94dd2307daf3cbbb98cac567fe5d86a37293
DTroczyk <DTroczyk@users.noreply.github.com>
2020-09-05 12:09:32 +0200

    Add LoginDto

---
+++ Api/Api.ViewModels/DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.ViewModels.DTOs
+{
+    public class LoginDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
