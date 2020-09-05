84d42eca7a48d83441cd4926ec1980301107378b
DTroczyk <DTroczyk@users.noreply.github.com>
2020-09-05 14:18:35 +0200

    Login hotfix
    A GET cannot have a body

--- Api/Api/Controllers/LoginController.cs
+++ Api/Api/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using Api.ViewModels.DTOs;
 using Api.ViewModels.ViewModel;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Api.Controllers
 {
@@ -17,32 +18,14 @@ namespace Api.Controllers
             _loginService = loginService;
         }
 
-        [HttpGet]
-        [Route("Body")]
-        public IActionResult Login([FromBody]LoginDto login)
+        [HttpPost]
+        public IActionResult Login(LoginDto login)
         {
             var user = _loginService.Login(login.username, login.password);
 
             if (user == null)
             {
                 return Unauthorized(new { message = "Username or password is incorrect.", status = 401});
-            }
-
-            var token = _loginService.GenerateJSONWebToken(user);
-            UserVm userVm = Mapper.Map<UserVm>(user);
-
-            return Ok(new { token = token, status = 200, user = userVm });
-        }
-
-        // Temporary GET
-        [HttpGet]
-        public IActionResult LoginWith(string username, string pass)
-        {
-            var user = _loginService.Login(username, pass);
-
-            if (user == null)
-            {
-                return Unauthorized(new { message = "Username or password is incorrect.", status = 401 });
             }
 
             var token = _loginService.GenerateJSONWebToken(user);
